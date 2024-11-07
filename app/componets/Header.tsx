1f1d40d9e06b9ad48e1ab6bc52ec94c3fe07c199
GitMing11 <132457273+GitMing11@users.noreply.github.com>
2024-11-07 17:23:49 +0900

    initial commit

---
+++ app/componets/Header.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="bg-[#111111] text-white shadow-md">
+      <nav className="max-w-7xl mx-auto px-4 py-4">
+        <ul className="flex flex-row space-x-6 justify-center">
+          <li>
+            <Link href="/" className="hover:text-[#901010] transition-colors">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/about"
+              className="hover:text-[#901010] transition-colors"
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/playlist"
+              className="hover:text-[#901010] transition-colors"
+            >
+              Playlist
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
--- app/playlist/page.tsx
+++ app/playlist/page.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function Playlist() {
   return (
     <main className="bg-[#202020] text-white min-h-screen flex flex-col items-center justify-center">
       {/* Hero Section */}
