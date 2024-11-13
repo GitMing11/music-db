86f26b491b2c10f495f3df15aebfdf8b36072fd5
GitMing11 <132457273+GitMing11@users.noreply.github.com>
2024-11-13 15:20:24 +0900

    검색 기록 저장

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import TrackList, { Track } from "@/app/components/TrackList";
 import { postClientCredentialsToken } from "@/app/api/spotify";
 import axios from "axios";
@@ -11,7 +11,20 @@ import { BASE_API_URL } from "@/app/api/url";
 export default function SearchPage() {
   const searchParams = useSearchParams();
   const query = searchParams?.get("query") || "";
+  const router = useRouter();
   const [tracks, setTracks] = useState<Track[]>([]);
+  const [recentSearches, setRecentSearches] = useState<string[]>([]);
+
+  // 최근 검색어 저장 함수
+  const saveRecentSearch = (newQuery: string) => {
+    // 이미 recentSearches에 동일한 검색어가 있으면 제거
+    setRecentSearches((prevSearches) => {
+      const updatedSearches = [newQuery, ...prevSearches.filter(search => search !== newQuery)];
+      // 최근 검색어를 localStorage에 저장
+      localStorage.setItem("recentSearches", JSON.stringify(updatedSearches));
+      return updatedSearches;
+    });
+  };
 
   // Spotify API를 통해 트랙 검색 함수
   const searchTracks = async (query: string) => {
@@ -44,10 +57,19 @@ export default function SearchPage() {
     }
   };
 
-  // 컴포넌트가 마운트되거나 query가 변경될 때 API 호출
+  // 컴포넌트가 마운트되었을 때 recentSearches를 localStorage에서 불러오기
+  useEffect(() => {
+    const storedRecentSearches = localStorage.getItem("recentSearches");
+    if (storedRecentSearches) {
+      setRecentSearches(JSON.parse(storedRecentSearches));
+    }
+  }, []);
+
+  // query가 변경될 때마다 검색하고, recentSearches에 추가
   useEffect(() => {
     if (query) {
       searchTracks(query);
+      saveRecentSearch(query);
     }
   }, [query]);
 
@@ -56,6 +78,27 @@ export default function SearchPage() {
       <h2 className="text-2xl font-semibold text-center text-white mb-4">
         Search Results for "{query}"
       </h2>
+      
+      {/* 최근 검색어 표시 */}
+      <div className="w-full max-w-4xl mb-4">
+        <h3 className="text-xl text-white mb-2">Recent Searches</h3>
+        <div className="flex flex-wrap gap-2">
+        {recentSearches.map((search, index) => (
+            <button
+              key={index}
+              onClick={() => {
+                // 클릭한 검색어로 URL을 업데이트
+                router.push(`?query=${search}`);
+              }}
+              className="bg-gray-800 text-white px-4 py-2 rounded-md hover:bg-gray-700"
+            >
+              {search}
+            </button>
+          ))}
+        </div>
+      </div>
+
+      {/* 트랙 리스트 */}
       <div className="w-full max-w-4xl">
         <TrackList tracks={tracks} />
       </div>
