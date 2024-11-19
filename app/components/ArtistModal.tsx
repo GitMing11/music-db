9a43cf52c89713b4d24bc8e411b2276346b1766e
yangshsl <shluxnsal01@gmail.com>
2024-11-19 12:53:29 +0900

    artist 페이지
    장르별 랜덤 아티스트, 아티스트 클릭시 모달창 나옴

---
+++ app/components/ArtistModal.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Link from 'next/link'; // Link 임포트 추가
+
+interface ArtistModalProps {
+  artist: any;
+  onClose: () => void;
+}
+
+const ArtistModal: React.FC<ArtistModalProps> = ({ artist, onClose }) => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75">
+      <div className="bg-[#1c1c1c] p-6 rounded-lg shadow-lg max-w-md w-full">
+        <button
+          onClick={onClose}
+          className="text-white float-right hover:text-red-500 transition-colors"
+        >
+          ✖
+        </button>
+        <h2 className="text-2xl font-semibold mb-2 text-center">
+          {artist.name}
+        </h2>
+        {artist.images && artist.images.length > 0 && (
+          <img
+            src={artist.images[0].url}
+            alt={artist.name}
+            className="w-full h-auto max-h-48 object-contain rounded-lg mb-4" // 모서리 라운드 처리
+          />
+        )}
+        <p className="text-white mb-2">Genres: {artist.genres.join(', ')}</p>
+        <p className="text-white mb-4">Popularity: {artist.popularity}</p>{' '}
+        {/* 인기도 추가 */}
+        <Link
+          href={`https://open.spotify.com/artist/${artist.id}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="bg-[#901010] text-white py-2 px-4 rounded-full transition-colors duration-300 hover:bg-[#b21212] text-center"
+        >
+          Visit on Spotify
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ArtistModal;
